d15e3d38f0e936fc5635ddc90dc1b72f0a5f0f8b
Sergio Gonzalez - Jefe BI <sergioandres.gonzalez@guapa.com.co>
2020-12-07 12:30:07 -0500

    ajuste fuente historia por bloque

--- app.py
+++ app.py
@@ -185,5 +185,5 @@ def actualizar_bloques(lote,year):
 
 if __name__ == '__main__':
     app.run_server(debug=False,host='0.0.0.0',port=8080)
-
+    #app.run_server()
 
--- data_processing.py
+++ data_processing.py
@@ -23,14 +23,27 @@ fechas_pendientes.query("aplicacion not in @lista_descartes",inplace=True)
 fechas_pendientes.drop_duplicates(subset=["aplicacion"],inplace=True,ignore_index=True)
 fechas_pendientes.drop(["bloque"],axis=1,inplace=True)
 
-historia_bloques = pd.read_excel("historia_por_bloque.xlsx")
-historia_bloques.columns=["id","lote","año","bloque","siembra","inducción PC","inducción SC","grupo","fecha apl PC","fecha apl SM1","fecha apl SC","fecha apl SM2","barrido"]
-historia_bloques["fecha apl PC"] = historia_bloques["fecha apl PC"].dt.date
-historia_bloques["fecha apl SM1"] = historia_bloques["fecha apl SM1"].dt.date
-historia_bloques["fecha apl SC"] = historia_bloques["fecha apl SC"].dt.date
-historia_bloques["fecha apl SM2"] = historia_bloques["fecha apl SM2"].dt.date
-historia_bloques["barrido"] = historia_bloques["barrido"].dt.date
-historia_bloques["siembra"]  = pd.to_datetime(historia_bloques["siembra"],infer_datetime_format=True).dt.date
-historia_bloques["inducción PC"]  = pd.to_datetime(historia_bloques["inducción PC"],infer_datetime_format=True).dt.date
-historia_bloques["inducción SC"]  = pd.to_datetime(historia_bloques["inducción SC"],infer_datetime_format=True).dt.date
+
+historia_bloques = pd.read_csv("historico_estados_blocks.csv",delimiter=";",parse_dates=["primera","produccion_semilla","segunda","produccion_semilla2","barrido"])
+historia_bloques.drop(["dif_fechas_pc_a_semillero","dif_fechas_semillero_a_sc","dif_fechas_sc_a_semillero","dif_fechas_semillero2_a_barrido"],axis=1,inplace=True)
+historia_bloques.rename(columns={"bloque":"id"},inplace=True)
+historia_bloques["lote"]=historia_bloques.id.str.slice(start=0,stop=2)
+historia_bloques["bloque"]=historia_bloques.id.str.slice(start=2,stop=4)
+historia_bloques["año"]=historia_bloques.id.str.slice(start=4,stop=6)
+historia_bloques.query("año!=''",inplace=True)
+historia_bloques["año"]=historia_bloques["año"].astype(int)
+columnas_de_fecha = ['primera', 'produccion_semilla', 'segunda', 'produccion_semilla2',"barrido"]
+for columna in columnas_de_fecha:
+    historia_bloques[columna]=historia_bloques[columna].dt.date
+historia_bloques = historia_bloques[['lote', 'bloque', 'año', 'primera', 'produccion_semilla','segunda','produccion_semilla2','barrido']]
+# historia_bloques = pd.read_excel("historia_por_bloque.xlsx")
+# historia_bloques.columns=["id","lote","año","bloque","siembra","inducción PC","inducción SC","grupo","fecha apl PC","fecha apl SM1","fecha apl SC","fecha apl SM2","barrido"]
+# historia_bloques["fecha apl PC"] = historia_bloques["fecha apl PC"].dt.date
+# historia_bloques["fecha apl SM1"] = historia_bloques["fecha apl SM1"].dt.date
+# historia_bloques["fecha apl SC"] = historia_bloques["fecha apl SC"].dt.date
+# historia_bloques["fecha apl SM2"] = historia_bloques["fecha apl SM2"].dt.date
+# historia_bloques["barrido"] = historia_bloques["barrido"].dt.date
+# historia_bloques["siembra"]  = pd.to_datetime(historia_bloques["siembra"],infer_datetime_format=True).dt.date
+# historia_bloques["inducción PC"]  = pd.to_datetime(historia_bloques["inducción PC"],infer_datetime_format=True).dt.date
+# historia_bloques["inducción SC"]  = pd.to_datetime(historia_bloques["inducción SC"],infer_datetime_format=True).dt.date
 #Reordenar columnas
